7bd9e576dfd7312bc4fd0a1b4f425f0b53ffd022
任天天 <ruattqwq@gmail.com>
2026-07-11 22:21:52 +0800

    refactor(ui): change message structs to avalonia object

--- FlowLauncher/Components/UI/RootLayoutViewModelMessageHost.cs
+++ FlowLauncher/Components/UI/RootLayoutViewModelMessageHost.cs
@@ -1,5 +1,7 @@
 using System.Windows.Input;
+using Avalonia;
 using Avalonia.Collections;
+using Avalonia.Metadata;
 using CommunityToolkit.Mvvm.Input;
 
 namespace FlowLauncher.Components.UI;
@@ -12,23 +14,75 @@ public enum MessageType
     Error,
 }
 
-public readonly record struct ToastMessage(
-    string Message,
-    MessageType Type = MessageType.Info,
-    ICommand? ClickCommand = null
-);
+public class MessageClickable : AvaloniaObject
+{
+    public static readonly StyledProperty<string> TextProperty =
+        AvaloniaProperty.Register<MessageClickable, string>(nameof(Text));
 
-public readonly record struct NotificationButton(
-    string Text,
-    ICommand? Command = null
-);
+    [Content]
+    public string Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
 
-public readonly record struct NotificationMessage(
-    string Message,
-    MessageType Type = MessageType.Info,
-    ICommand? Command = null,
-    IReadOnlyList<NotificationButton>? Buttons = null
-);
+    public static readonly StyledProperty<ICommand?> CommandProperty =
+        AvaloniaProperty.Register<MessageClickable, ICommand?>(nameof(Command));
+
+    public ICommand? Command
+    {
+        get => GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    public static readonly StyledProperty<object?> CommandParameterProperty =
+        AvaloniaProperty.Register<MessageClickable, object?>(nameof(CommandParameter));
+
+    public object? CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+}
+
+public class ToastMessage : MessageClickable
+{
+    public static readonly StyledProperty<MessageType> TypeProperty =
+        AvaloniaProperty.Register<ToastMessage, MessageType>(nameof(Type));
+
+    public MessageType Type
+    {
+        get => GetValue(TypeProperty);
+        set => SetValue(TypeProperty, value);
+    }
+}
+
+public class NotificationButton : MessageClickable;
+
+public class NotificationMessage : MessageClickable
+{
+    public static readonly StyledProperty<MessageType> TypeProperty =
+        AvaloniaProperty.Register<NotificationMessage, MessageType>(nameof(Type));
+
+    public MessageType Type
+    {
+        get => GetValue(TypeProperty);
+        set => SetValue(TypeProperty, value);
+    }
+
+    public static readonly DirectProperty<NotificationMessage, List<NotificationButton>> ButtonsProperty =
+        AvaloniaProperty.RegisterDirect<NotificationMessage, List<NotificationButton>>(
+            nameof(Buttons),
+            o => o.Buttons,
+            (o, v) => o.Buttons = v
+        );
+
+    public List<NotificationButton> Buttons
+    {
+        get => field ??= [];
+        set => SetAndRaise(ButtonsProperty, ref field!, value);
+    }
+}
 
 partial class RootLayoutViewModel
 {
@@ -40,7 +94,6 @@ partial class RootLayoutViewModel
         /// 发送 Toast 提示
         /// </summary>
         /// <param name="message">提示内容</param>
-        [RelayCommand]
         public void SendToast(ToastMessage message)
         {
         }
@@ -49,7 +102,6 @@ partial class RootLayoutViewModel
         /// 发送通知消息
         /// </summary>
         /// <param name="message">消息内容</param>
-        [RelayCommand]
         public void SendNotification(NotificationMessage message)
         {
         }
